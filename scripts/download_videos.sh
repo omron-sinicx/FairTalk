86e56dd3e3d0b892ce627117bd3ebad7088918ac
AtsushiHashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2022-07-05 10:28:17 +0900

    modified

--- scripts/download_videos.sh
+++ scripts/download_videos.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 CONFIG_DIR="$SCRIPT_DIR/../config"
 DEST_DIR="$SCRIPT_DIR/../data"
 for subset in "train" "val" "test"; do
-    dat="$CONFIG_DIR/video_list_$subset.dat"
-    echo download $dat
-    yt-dlp --batch-file $dat --output  "$DEST_DIR/$subset/$subset_vimeo_%(id)09d.mp4"
+    dat="${CONFIG_DIR}/video_list_${subset}.dat"
+    echo download ${dat}
+    yt-dlp --batch-file ${dat} --output  "${DEST_DIR}/${subset}/${subset}_vimeo_%(id)09d.mp4"
 done
