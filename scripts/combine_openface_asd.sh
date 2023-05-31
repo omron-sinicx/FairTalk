261e00985331afbebcd97cba1d702853c191996c
Atsushi Hashimoto <atsushi.hashimoto@sinicx.com>
2023-05-31 15:14:13 +0100

    add train_youtube.

--- scripts/combine_openface_asd.sh
+++ scripts/combine_openface_asd.sh
@@ -5,43 +5,41 @@ if [ $# -gt 0 ]; then
     cpu_num=$1
 fi
     
+MAX_CONCURRENT_JOBS=4 
+running_jobs_count() {
+  jobs -r | wc -l
+}
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 SRC_DIR="$SCRIPT_DIR/../data"
 PYTHON_SCRIPT="$SCRIPT_DIR/combine.py"
-subsets=("train" "val" "test")
 
-# parallelize for loop in a smart way!
-# https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop
-# initialize a semaphore with a given number of tokens
-open_sem(){
-    mkfifo pipe-$$
-    exec 3<>pipe-$$
-    rm pipe-$$
-    local i=$1
-    for((;i>0;i--)); do
-        printf %s 000 >&3
+# apply to youtube training dataset
+openface_dir=$SRC_DIR/openface_features/train/
+asd_dir=$SRC_DIR/train
+for video in $(find $SRC_DIR/train/train_youtube* -maxdepth 0 -type f); do
+    while (( $(running_jobs_count) >= MAX_CONCURRENT_JOBS )); do
+      sleep 5
     done
-}
+    dir=$(dirname $video)
+    base=$(basename $video ".mp4")
 
-# run the given command asynchronously and pop/push tokens
-run_with_lock(){
-    local x
-    # this read waits until there is something to read
-    read -u 3 -n 3 x && ((0==x)) || exit $x
-    (
-     ( "$@"; )
-    # push the return code of the command to the semaphore
-    printf '%.3d' $? >&3
-    )&
-}
-
-open_sem $cpu_num
+    completion_check=$dir/$base/.combined
+    if [ -e $completion_check ]; then
+        echo "skip (already completed)"
+    else
+        python3 $PYTHON_SCRIPT -f $openface_dir/$base -a $dir/$base -o $dir/$base && touch $completion_check
+    fi
+done
+exit
 
 for subset in "train" "val" "test"; do
     openface_dir=$SRC_DIR/openface_features/$subset/
     asd_dir=$SRC_DIR/$subset
     for video in $(ls -1 $SRC_DIR/$subset/*.mp4); do
+        while (( $(running_jobs_count) >= MAX_CONCURRENT_JOBS )); do
+          sleep 5
+        done
         dir=$(dirname $video)
         base=$(basename $video ".mp4")
         
@@ -49,7 +47,7 @@ for subset in "train" "val" "test"; do
         if [ -e $completion_check ]; then
             echo "skip (already completed)"
         else
-            run_with_lock python3 $PYTHON_SCRIPT -f $openface_dir/$base -a $dir/$base -o $dir/$base
+            python3 $PYTHON_SCRIPT -f $openface_dir/$base -a $dir/$base -o $dir/$base && touch $completion_check
         fi
     done
 done
